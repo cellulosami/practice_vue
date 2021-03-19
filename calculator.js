abc573b82c12573e75a4bcbb0330390104fb98dd
austin shields <ajshields196@gmail.com>
2021-03-18 22:26:36 -0400

    add sum function to calculator

--- calculator.js
+++ calculator.js
@@ -1,8 +1,26 @@
 var app = new Vue({
   el: "#app",
-  data: function() {
-    return {
-      message: "Hello Vue!"
-    };
+  data: {
+    message: "Hello Vue!",
+    number1: "",
+    number2: "",
+    number3: "",
+    sum: "",
+    resultText: ""
+  },
+  methods: {
+    calculateSum: function() {
+      if (isNaN(parseInt(this.number1) + parseInt(this.number2) + parseInt(this.number3))) {
+        this.sum = "";
+        this.resultText = "<<Invalid input>>";
+      } else {
+        this.sum = parseInt(this.number1) + parseInt(this.number2) + parseInt(this.number3);
+        this.resultText = "The sum is:";
+      }
+    }
   }
 });
+
+//on-click button
+//add 3 numbers together
+//display sum
